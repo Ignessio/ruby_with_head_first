91f2395faab098cf7dda12958977695175589825
valentin_zet <xerrox@yandex.ru>
2021-02-14 11:07:37 +0300

    ruby with head first

---
+++ get_number.rb
@@ -0,0 +1,45 @@
+# Get My Number Game
+# Written by Valentin
+
+puts "Welcome to 'Get My Number!'"
+
+#Get player name and print greeting
+print "What's your name? "
+name = gets.strip
+puts "Welcome, #{name}!"
+
+#Save the random number
+puts "I've got a random number between 1 and 100"
+puts "Can you guess it?"
+target = rand(100) + 1
+
+#Count attempts
+num_guesses = 0
+
+#Check end game
+guessed_it = false
+
+until num_guesses == 10 || guessed_it
+    
+puts "You've got #{10 - num_guesses} guesses left."
+print "Make a guess: "
+guess = gets.to_i
+
+num_guesses += 1
+
+#Compare entered and random numbers. Print result
+if guess < target
+    puts "Oops. Your guess was LOW."
+elsif guess > target
+    puts "Oops. Your guess was HIGH."
+elsif guess == target
+    puts "Good job, #{name}!"
+    puts "You guessed my number in #{num_guesses} guesses!"
+    guessed_it = true
+end
+end
+
+#If no more attempts print random number
+unless guessed_it
+    puts "Sorry. You didn't get my number. (It was #{target}.)"
+end
