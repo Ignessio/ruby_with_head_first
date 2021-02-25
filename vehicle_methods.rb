f609aa7c008ffe08d39afba645f80f6728bc689b
valentin_zet <xerrox@yandex.ru>
2021-02-25 08:13:55 +0300

    vehicle_methods updated, camera file added

--- vehicle_methods.rb
+++ vehicle_methods.rb
@@ -1,18 +1,45 @@
-def accelerate
-    puts "Stepping on the gas"
-    puts "Speeding up"
+class Vehicle
+    attr_accessor :odometer, :gas_used
+    def accelerate
+        puts "Floor it!"
+    end
+
+    def sound_horn
+        puts "Beep! Beep!"
+    end
+    def steer
+        puts "Turn 2 front wheels."
+    end
+    def mileage
+        return @odometer / @gas_used
+    end
 end
 
-def sound_horn
-    puts "Pressing the horn button"
-    puts "Beep beep!"
+class Car < Vehicle
 end
 
-def use_headlights(brightness = "low-beam")
-    puts "Turning on #{brightness} headlights"
-    puts "Watch out for deer!"
+class Truck < Vehicle
+    attr_accessor :cargo
+    def load_bed(contents)
+        puts "Securing #{contents} in the truck bed."
+        @cargo = contents
+    end
+end
+class Motorcycle < Vehicle
+    def steer
+        puts "Turn front wheel."
+    end
 end
 
-sound_horn
-accelerate
-use_headlights("high-beam")
+truck = Truck.new
+truck.accelerate
+truck.steer
+truck.load_bed("259 bouncy balls")
+puts "The truck is carrying #{truck.cargo}."
+car = Car.new
+car.odometer = 11432
+car.gas_used = 366
+puts car.instance_variables
+puts "Lifetime MPG: #{car.mileage}"
+motorcycle = Motorcycle.new
+motorcycle.steer
