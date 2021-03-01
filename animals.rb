ee7d967c186028e472e7162c7a4d9f7edb2ba379
valentin_zet <xerrox@yandex.ru>
2021-03-01 17:39:32 +0300

    vehicle renamed, animals modified

--- animals.rb
+++ animals.rb
@@ -24,6 +24,9 @@ class Animal
 end
 
 class Dog < Animal
+    def to_s
+        "#{@name} the dog, age #{age}"
+    end
 end
 
 class Bird < Animal
@@ -59,3 +62,11 @@ dillon = Armadillo.new
 dillon.name = "Dillon"
 dillon.move("burrow")
 dillon.talk
+
+lucy = Dog.new
+lucy.name = "Lucy"
+lucy.age = 4
+rex = Dog.new
+rex.name = "Rex"
+rex.age = 2
+puts lucy, rex
