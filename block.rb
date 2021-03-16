968f709e079358bfa2bd9d5524a7de0b796f7608
valentin_zet <xerrox@yandex.ru>
2021-03-16 20:05:17 +0300

    block file modified

--- block.rb
+++ block.rb
@@ -1,9 +1,18 @@
-def my_method(&my_block)
+def my_method
     puts "We're in the method, about to invoke your block!"
-    my_block.call
+    yield
     puts "We're back in the method!"
 end
 
 my_method do
     puts "We're in the block!"
+end
+
+def give
+    yield "2 turtle doves", "1 partridge"
+end
+
+give do |present1, present2|
+    puts "My method gave to me..."
+    puts present1, present2
 end
