c044826b199b092e88238a16e0d3f2fd34828be3
valentin_zet <xerrox@yandex.ru>
2021-03-30 18:54:33 +0300

    test file modified

--- test.rb
+++ test.rb
@@ -1,35 +1,5 @@
-=begin
-def make_casserole
-    puts "Preheat oven to 375 degrees"
-    ingredients = yield
-    puts "Place #{ingredients} in dish"
-    puts "Bake for 20 minutes"
-end
-
-make_casserole do
-    "noodles, celery, and tuna"
-end
-
-make_casserole do
-    "rice, broccoli, and chicken"
-end
-
-x = gets.to_i
-y = gets.to_i
-while x <= y
-puts x
-x += 1
-end
-
-=end
-
-n = gets.to_i
-
-# сотни
-p n / 100 + n / 10 % 10 + n % 10
-
-# десятки
-#b = n / 10 % 10
-
-# единицы
-#c = n % 10
+st = ["s", "t", "a", "t", "u", "s"]
+puts st.join
+st << "!"
+puts st.last
+print st
