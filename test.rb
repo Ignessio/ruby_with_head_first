0122ac9a0b39b45135146d6cee110ad920330fbb
valentin_zet <xerrox@yandex.ru>
2021-04-16 16:36:44 +0300

    char_counter file added

--- test.rb
+++ test.rb
@@ -28,7 +28,6 @@ print arr1
 hash = {"Январь"=>31, "Февраль"=>28, "Март"=>31}
 puts hash
 
-=end
 
 
 def x(y)
@@ -37,3 +36,22 @@ def x(y)
     res
   end
   puts x(3)
+
+"performance".each_char do |x|
+  puts x
+end
+
+hash = {one:1, two:2, three:3, four:4, five:5, six:6}
+sum = 0
+hash.each { |key, value| sum += value }
+puts sum
+=end
+
+text = "I am learning Ruby and it is fun!"
+text.downcase!
+freqs = {}
+freqs.default = 0
+
+text.each_char { |char| freqs[char] += 1}
+
+("a".."z").each {|x| puts "#{x} : #{freqs[x]}" }
