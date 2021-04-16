3deac5bf8f851729ecafadea06c9d409c0a2bef6
valentin_zet <xerrox@yandex.ru>
2021-04-16 09:12:00 +0300

    each modified, volume added

--- each.rb
+++ each.rb
@@ -1,2 +1,2 @@
-["foo", "bar", "baz"].each_with_index { |value, index| puts "#{index}: #{value}" }
-{foo:1, bar:2, baz:3}.each { |key, value| puts "#{key}: #{value}" }
+["foo", "bar", "baz"].each_with_index { |value, index| puts "index-#{index}: #{value}" }
+{foo:1, bar:2, baz:3}.each { |key, value| puts "key-#{key}: #{value}" }
