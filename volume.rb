61f3eb646d484acf68a3d5f9f79ff00d4ea18bfe
valentin_zet <xerrox@yandex.ru>
2021-05-16 07:11:13 +0300

    volume file modified

--- volume.rb
+++ volume.rb
@@ -1,6 +1,6 @@
 def volume(options)
-    options[:depth] * options[:height] * options[:width]
+    puts "Square is #{options[:height] * options[:width]} "
+    puts "Volume is #{options[:depth] * options[:height] * options[:width]} "
 end
 
 result = volume( width: 10, height: 5, depth: 2.5 )
-puts "Volume is #{result}"
