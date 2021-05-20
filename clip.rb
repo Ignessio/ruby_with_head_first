7a44c34e38238946e0b668d8614b925581108e1a
valentin_zet <xerrox@yandex.ru>
2021-05-20 20:59:23 +0300

    clip file modified using module

--- clip.rb
+++ clip.rb
@@ -1,27 +1,39 @@
-class Clip
-    attr_reader :comments
-    
-    def initialize
-        @comments = []
+module AcceptsComments
+    def comments
+        if @comments
+            @comments
+        else
+            @comments = []
+        end
     end
-
     def add_comment(comment)
         comments << comment
     end
+end
 
+class Clip
     def play
-        puts "Playing #{object_id}..."
+        puts "Playing #{object_id}"
     end
-
 end
 
 class Video < Clip
+    include AcceptsComments
     attr_accessor :resolution
 end
 
 class Song < Clip
+    include AcceptsComments
     attr_accessor :beats_per_minute
 end
+
+class Photo
+    include AcceptsComments
+    def show
+        puts "Displaying #{object_id}"
+    end
+end
+
 
 video = Video.new
 video.add_comment("Cool slow motion effect!")
@@ -30,3 +42,11 @@ song = Song.new
 song.add_comment("Avesome beat.")
 
 p video.comments, song.comments
+video.play
+song.play
+
+photo = Photo.new
+photo.add_comment("Beautiful colors.")
+
+p photo.comments
+photo.show
