4ff30eb99349787faaff4ca915cfc826fbdb888c
valentin_zet <xerrox@yandex.ru>
2021-05-26 07:25:02 +0300

    test file updated

--- test.rb
+++ test.rb
@@ -72,7 +72,6 @@ def fib(num)
 end
 puts fib(4)
 
-=end
 
 class Calc
   def self.sqr(x)
@@ -81,3 +80,90 @@ class Calc
 end
 
 Calc.sqr(2)
+
+class Player
+  attr_accessor :name, :health, :power
+  def initialize(n, h, pow)
+    @name = n
+    @health = h
+    @power = pow
+  end
+  def isAlive
+    @health > 0
+  end
+  def hit(opponent)
+    opponent.health -= self.power
+  end
+  def to_s
+    "#{name}: Health: #{health}, Power: #{power}"
+  end
+end
+
+def fight(p1, p2)
+  while p1.isAlive && p2.isAlive
+    p1.hit(p2)
+    p2.hit(p1)
+    show_info(p1, p2)
+  end
+    
+  if p1.isAlive 
+    puts "#{p1.name} WON!" 
+  elsif p2.isAlive
+    puts "#{p2.name} WON!" 
+  else
+    puts "TIE!"
+  end
+end
+
+def show_info(*p)
+  p.each { |x| puts x}
+end
+
+p1 = Player.new("Player 1", 1+rand(100), 1+rand(20))
+p2 = Player.new("Player 2", 1+rand(100), 1+rand(20))
+
+# показываем информацию об игроках
+show_info(p1, p2)
+
+puts "LETS FIGHT!"
+fight(p1, p2)
+
+class Custom
+def initialize x
+  @x = x
+end
+def to_s
+  @x
+end
+end
+x = gets
+p = Custom.new(x)
+puts p
+=end
+
+module Car
+  class Volvo
+    def initialize
+      @@wheels = 4
+    end
+    def how_many_wheels
+      puts "#{@@wheels}"
+    end
+  end
+end
+
+module Truck
+  class Volvo
+    def initialize
+      @@wheels = 6
+    end
+    def how_many_wheels
+      puts "#{@@wheels}"
+    end
+  end
+end
+
+car = Car::Volvo.new
+car.how_many_wheels
+car = Truck::Volvo.new
+car.how_many_wheels
