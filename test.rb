52ef60be9ed447d1b77b953020100bba9cf3367e
valentin_zet <xerrox@yandex.ru>
2021-06-10 08:37:09 +0300

    oven and test files updated

--- test.rb
+++ test.rb
@@ -168,3 +168,14 @@ car = Truck::Volvo.new
 car.how_many_wheels
 =end
 
+def drive(destination)
+  if destination == "Hawaii"
+    raise "You can't drive to Hawaii!"
+  end
+end
+
+begin
+drive("Hawaii")
+rescue => error
+  puts error.message
+end
