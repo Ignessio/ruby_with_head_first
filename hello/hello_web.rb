d86487c5c303bf48bee2de335b48174197d1dfd2
valentin_zet <xerrox@yandex.ru>
2021-07-14 18:36:35 +0300

    html files added

---
+++ hello/hello_web.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get ('/hello') do
+    'Hello web!'
+end
+
+get ('/yes') do
+    'Yes, we are goint to the park!'
+end
+
+get ('/no') do
+    'No, they do not comming soon!'
+end
+
+get ('/sandwich') do
+    'Make your own sandwich!'
+end
